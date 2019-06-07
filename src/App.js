1a93fcadd7ef9f91f8b688677fed6eceaf4585de
Armando Ruiz <armando.ruiz@industrie.co>
2019-06-07 12:27:09 +1000

    Creando el context, el Provider y el Consumer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React { Fragment } from 'react';
+import React, { Fragment } from 'react';
 import Header from './components/Header';
+import CategoriasProvider from './context/CategoriasContext';
+// en realidad importamos la clase, no el Provider en si.
+
 
 function App() {
   return (
-    <Fragment>
+    <CategoriasProvider>
+      {/* El provider wrap otro componente que es donde se van a poder usar los datos
+      hay que anadir el childern en el provider component los datos de categorias van a estar disponibles en el header*/}
       <Header />
-    </Fragment>
+    </CategoriasProvider>
   );
 }
 
---
+++ src/context/CategoriasContext.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+// Crear el context
+const CategoriasContext = React.createContext();
+// Consumer
+export const CategoriasConsumer = CategoriasContext.Consumer;
+
+class CategoriasProvider extends Component {
+
+  token = 'V6N5WXHCFDL3NU73VIYH';
+
+  state = {
+    categorias: [],
+  }
+
+  //Una vez cargue el componente se va a hacer la consulta por eso usamos componentDidMount
+  componentDidMount() {
+    this.obtenerCategorias();
+  }
+
+  obtenerCategorias = async () => {
+    let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=en_AU`
+
+    let categorias = await axios.get(url);
+
+    console.log(categorias.data.categories);
+    // Cambiar el state
+    this.setState({
+      categorias: categorias.data.categories
+    })
+  };
+
+  render() {
+    return (
+      <CategoriasContext.Provider
+        value= {{
+          categorias: this.state.categorias,
+        }}
+      > 
+        {this.props.children}
+        {/* para que el componente hijo pueda leer los datos, en este caso los datos son categorias que le pasamos al componente header */}
+      </CategoriasContext.Provider>
+    );
+  }
+}
+
+export default CategoriasProvider;
